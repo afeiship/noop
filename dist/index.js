a0686c5cb37b5e0f9cfa02d29d464146f06a40c9
afeiship <1290657123@qq.com>
2017-08-11 15:41:44 +0800

    issue: refactor

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var NOOP = function NOOP() {};
+var NOOP_STRING = '';
+var NOOP_ARRAY = [];
+var NOOP_OBJECT = {};
+
+module.exports = NOOP;
+exports.default = NOOP;
+exports.NOOP_STRING = NOOP_STRING;
+exports.NOOP_ARRAY = NOOP_ARRAY;
+exports.NOOP_OBJECT = NOOP_OBJECT;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+const NOOP = function(){};
+const NOOP_STRING = '';
+const NOOP_ARRAY = [];
+const NOOP_OBJECT = {};
+
+module.exports = NOOP;
+export default NOOP;
+
+export {
+  NOOP_STRING,
+  NOOP_ARRAY,
+  NOOP_OBJECT
+};
