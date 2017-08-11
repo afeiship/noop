00af5d5f69a8520d0fb0bd865a91ce8fd950545b
afeiship <1290657123@qq.com>
2017-08-11 15:44:24 +0800

    issue: add comments

--- dist/index.js
+++ dist/index.js
@@ -8,8 +8,12 @@ var NOOP_STRING = '';
 var NOOP_ARRAY = [];
 var NOOP_OBJECT = {};
 
+// export default:
 module.exports = NOOP;
 exports.default = NOOP;
+
+// export string/array/object
+
 exports.NOOP_STRING = NOOP_STRING;
 exports.NOOP_ARRAY = NOOP_ARRAY;
 exports.NOOP_OBJECT = NOOP_OBJECT;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const NOOP_STRING = '';
 const NOOP_ARRAY = [];
 const NOOP_OBJECT = {};
 
+// export default:
 module.exports = NOOP;
 export default NOOP;
 
+// export string/array/object
 export {
   NOOP_STRING,
   NOOP_ARRAY,
